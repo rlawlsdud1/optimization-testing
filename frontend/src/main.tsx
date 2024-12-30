69190d98b8c2fdb59317479e0dc6cb188ed0f4da
KIMJINYOUNG <k5444862@naver.com>
2024-12-30 17:25:24 +0900

    feat: 캐시 모니터링 및 디버깅을 위한 React Query Devtools 설치

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <App />
+      <ReactQueryDevtools initialIsOpen={true} />
     </BrowserRouter>
   </QueryClientProvider>
   // </StrictMode>
