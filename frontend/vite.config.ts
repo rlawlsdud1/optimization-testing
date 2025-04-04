870f24e22ab264c4dca843c404e93687b61d7f59
KIMJINYOUNG <k5444862@naver.com>
2025-04-04 19:30:32 +0900

    feat: 청크 분할 시각화를 위한 rollup-plugin-visualizer 설치

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import visualizer from "./node_modules/rollup-plugin-visualizer/dist/plugin/index.d";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    visualizer({
+      filename: "dist/stats.html",
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
+});
